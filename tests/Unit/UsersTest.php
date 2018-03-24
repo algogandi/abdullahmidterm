a365bbb25b39e953de667cbe4ff9bec0c898e3c2
Abdullah Algogandi <asa89@njit.edu>
2018-03-24 14:05:04 -0400

    making the register test

---
+++ tests/Unit/UsersTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UsersTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        //$user = User::inRandomOrder()->first();
+        //dd($user);
+        //$this->assertInternalType('int' ,$user->id);
+        //$this->assertInstanceOf('App\User' ,$user);   //clas path for App
+
+        $this->assertTrue(true);    }
+}
