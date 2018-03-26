e6856ce0fe79746a067d5acc40c52f2586ecdae5
Abdullah Algogandi <asa89@njit.edu>
2018-03-26 16:48:36 -0400

    Insert user into the users table Testing

--- tests/Unit/UsersTest.php
+++ tests/Unit/UsersTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class UsersTest extends TestCase
 {
@@ -13,12 +14,29 @@ class UsersTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testInsert()
     {
-        //$user = User::inRandomOrder()->first();
-        //dd($user);
-        //$this->assertInternalType('int' ,$user->id);
-        //$this->assertInstanceOf('App\User' ,$user);   //clas path for App
 
-        $this->assertTrue(true);    }
+        $user = new User();
+        $user->name = '';
+        $user->email = '';
+        $user->password = '';
+        //$user->save();
+
+        //$this->assertTrue(true);
+
+        $this->assertTrue($user->save());
+
+    }
 }
+
+
+//$user = User::inRandomOrder()->first();
+//dd($user);
+//$this->assertInternalType('int' ,$user->id);
+//$this->assertInstanceOf('App\User' ,$user);   //clas path for App
+
+
+//$user = new User();
+//$user->name = $request->name;
+//$user->save();
