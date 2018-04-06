f9c7110edcf999d4b73b2d1047b35c46baef2349
Abdullah Algogandi <asa89@njit.edu>
2018-04-06 15:17:20 -0400

    removed username from the Auth make

--- app/Car.php
+++ app/Car.php
@@ -4,6 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Car
+ *
+ * @property int $id
+ * @property string $make
+ * @property string $model
+ * @property string $year
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Car whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Car whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Car whereMake($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Car whereModel($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Car whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Car whereYear($value)
+ * @mixin \Eloquent
+ */
 class Car extends Model
 {
     //
