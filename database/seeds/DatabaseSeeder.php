af2e1fb5d598824504e72522cce5e8482494902e
Abdullah Algogandi <asa89@njit.edu>
2018-04-07 11:52:10 -0400

    Added ANSWER to the project and test for user profile and ANSWER test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,10 @@ class DatabaseSeeder extends Seeder
          $this->call(UsersTableSeeder::class);
         $this->call(CarsTableSeeder::class);
         $this->call(QuestionTableSeeder::class);
+        $this->call(AnswersTableSeeder::class);
+
+
+
 
 
     }
