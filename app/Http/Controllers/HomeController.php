0b6d2e92728be16f15ca653a3d93df28657a6333
Abdullah Algogandi <asa89@njit.edu>
2018-05-02 21:06:24 -0400

    working in the controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,11 +35,9 @@ class HomeController extends Controller
     public function admin(Request $req){
         return view('middleware')->withMessage("Admin");
     }
-
     public function super_admin(Request $req){
         return view('middleware')->withMessage("Super Admin");
     }
-
     public function member(Request $req){
         return view('middleware')->withMessage("Member");
     }
